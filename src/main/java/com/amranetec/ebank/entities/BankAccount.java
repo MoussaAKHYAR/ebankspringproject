eeee964da895cf499cf182510fb8136eef6efebb
Moussa AKHYAR <moussa.akhyar@wineos-group.com>
2022-05-24 13:24:33 +0000

    migration vers la base de donnée mysql

--- src/main/java/com/amranetec/ebank/entities/BankAccount.java
+++ src/main/java/com/amranetec/ebank/entities/BankAccount.java
@@ -10,10 +10,10 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-//@DiscriminatorColumn(name = "TYPE", length = 4)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE", length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
-public abstract class BankAccount {
+public class BankAccount {
     @Id
     private String id;
     private double balance;
