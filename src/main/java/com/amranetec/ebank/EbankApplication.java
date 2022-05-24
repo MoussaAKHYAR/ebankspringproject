49ff308214bde7d9cb45b9918bf8a6fa85913436
Moussa AKHYAR <moussa.akhyar@wineos-group.com>
2022-05-24 10:53:27 +0000

    test d'insertion des customers dans la base

--- src/main/java/com/amranetec/ebank/EbankApplication.java
+++ src/main/java/com/amranetec/ebank/EbankApplication.java
@@ -1,11 +1,15 @@
 package com.amranetec.ebank;
 
+import com.amranetec.ebank.entities.Customer;
 import com.amranetec.ebank.repositories.AccountOperationRepository;
 import com.amranetec.ebank.repositories.BankAccountRepository;
 import com.amranetec.ebank.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EbankApplication {
@@ -14,12 +18,18 @@ public class EbankApplication {
         SpringApplication.run(EbankApplication.class, args);
     }
 
+    @Bean
     CommandLineRunner start(
             CustomerRepository customerRepository,
             BankAccountRepository bankAccountRepository,
             AccountOperationRepository accountOperationRepository){
         return args -> {
-
+            Stream.of("Moussa","Amrane","Youssouf").forEach(name ->{
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                customerRepository.save(customer);
+            });
         };
     }
 }
