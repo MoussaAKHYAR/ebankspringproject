71c6cb5e9ba7bcb29022d335c45bc2a0276b34a4
Moussa AKHYAR <moussa.akhyar@wineos-group.com>
2022-05-24 11:44:55 +0000

    test d'insertion des customers et bankAccount avec heritage table_per_class

--- src/main/java/com/amranetec/ebank/entities/BankAccount.java
+++ src/main/java/com/amranetec/ebank/entities/BankAccount.java
@@ -10,10 +10,10 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "TYPE", length = 4)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+//@DiscriminatorColumn(name = "TYPE", length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
-public class BankAccount {
+public abstract class BankAccount {
     @Id
     private String id;
     private double balance;
